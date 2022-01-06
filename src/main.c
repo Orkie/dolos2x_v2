f12c989e69e079564ab405d28b6e4b1c2f4a023f
Adan Scotney <adan.scotney@gmail.com>
2022-01-06 19:11:40 +0000

    Add SDL dependency

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <SDL.h>
 #include <cpu.h>
 #include <instr.h>
 
@@ -16,5 +17,12 @@ int main() {
   
   uint32_t result = _petraea_eval_operand2(&cpu, &op, &carryValid, &carry);
   printf("result: 0x%x\n", result);
+
+  SDL_Window* sdlWindow = SDL_CreateWindow("dolos2x", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 960, 0);
+  SDL_Renderer* sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_ACCELERATED);
+
+  SDL_DestroyRenderer(sdlRenderer);
+  SDL_DestroyWindow(sdlWindow);
+  SDL_Quit();
 }
 
