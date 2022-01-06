c10cdbf9a3d49970538bccfd05cb7a05bfeef5f3
Adan Scotney <adan.scotney@gmail.com>
2022-01-06 19:07:20 +0000

    Test of build system

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <cpu.h>
+#include <instr.h>
 
 int main() {
-    printf("hello\n");
+  pt_arm_cpu cpu;
+  pt_arm_init_cpu(&cpu, ARM920T, NULL, NULL, NULL, NULL, NULL, NULL);
+  bool carryValid;
+  bool carry;
+
+  pt_arm_operand2 op;
+  op.is_immediate = true;
+  op.op.imm.value = 0x123456;
+  op.op.imm.carryValid = true;
+  op.op.imm.carry = false;
+  
+  uint32_t result = _petraea_eval_operand2(&cpu, &op, &carryValid, &carry);
+  printf("result: 0x%x\n", result);
 }
 
