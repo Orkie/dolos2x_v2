4b9f9a395c745f639061a92bcc730f7da0f99590
Adan Scotney <adan.scotney@gmail.com>
2022-01-08 11:34:38 +0000

    Use new petraea bus API

--- src/main.c
+++ src/main.c
@@ -10,35 +10,41 @@ static struct termios old_tio, new_tio;
 #define GP2X_RAM_SIZE (64 * 1024 * 1024)
 
 void* gp2x_ram;
+void* gp2x_mmio;
 
-uint32_t bus_fetch_word(uint32_t addr) {
-  if(addr >= GP2X_RAM_SIZE) { fprintf(stderr, "Tried to read 0x%x\n", addr); }
-  return ((uint32_t*)gp2x_ram)[addr>>2];
+// TODO - when arm940 is added, these need to block on non-IO (i.e. RAM) accesses while the other core has the bus
+// TODO - callback notifies upon read or write
+
+int bus_fetch(uint32_t addr, int bytes, void* ret) {
+  if(addr < GP2X_RAM_SIZE) {
+    if(bytes == 4) {
+      *((uint32_t*)ret) = ((uint32_t*)gp2x_ram)[addr>>2];
+    } else if(bytes == 2) {
+      *((uint16_t*)ret) = ((uint16_t*)gp2x_ram)[addr>>1];
+    } else {
+      *((uint8_t*)ret) = ((uint8_t*)gp2x_ram)[addr];
+    }
+    return 0;
+  } else {
+    fprintf(stderr, "Tried to write unmapped address 0x%x\n", addr);
+    return -1;
+  }
 }
 
-uint16_t bus_fetch_halfword(uint32_t addr) {
-  if(addr >= GP2X_RAM_SIZE) { fprintf(stderr, "Tried to read 0x%x\n", addr); }
-  return ((uint16_t*)gp2x_ram)[addr>>1];
-}
-
-uint8_t bus_fetch_byte(uint32_t addr) {
-  if(addr >= GP2X_RAM_SIZE) { fprintf(stderr, "Tried to read 0x%x\n", addr); }
-  return ((uint8_t*)gp2x_ram)[addr];
-}
-
-void bus_write_word(uint32_t addr, uint32_t value) {
-  if(addr >= GP2X_RAM_SIZE) { fprintf(stderr, "Tried to read 0x%x 0x%x\n", addr, value); }
-  ((uint32_t*)gp2x_ram)[addr>>2] = value;
-}
-
-void bus_write_halfword(uint32_t addr, uint16_t value) {
-  if(addr >= GP2X_RAM_SIZE) { fprintf(stderr, "Tried to write 0x%x 0x%x\n", addr, value); }
-  ((uint16_t*)gp2x_ram)[addr>>1] = value;
-}
-
-void bus_write_byte(uint32_t addr, uint8_t value) {
-  if(addr >= GP2X_RAM_SIZE) { fprintf(stderr, "Tried to read 0x%x 0x%x\n", addr, value); }
-  ((uint8_t*)gp2x_ram)[addr] = value;
+int bus_write(uint32_t addr, int bytes, void* value) {
+  if(addr < GP2X_RAM_SIZE) {
+    if(bytes == 4) {
+      ((uint32_t*)gp2x_ram)[addr>>2] = *((uint32_t*)value);
+    } else if(bytes == 2) {
+      ((uint16_t*)gp2x_ram)[addr>>1] = *((uint16_t*)value);
+    } else {
+      ((uint8_t*)gp2x_ram)[addr] = *((uint8_t*)value);
+    }
+    return 0;
+  } else {
+    fprintf(stderr, "Tried to write unmapped address 0x%x\n", addr);
+    return -1;
+  }
 }
 
 void clock(pt_arm_cpu* arm920) {
@@ -53,13 +59,13 @@ int main() {
   new_tio.c_lflag &=(~ICANON & ~ECHO);
   tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
   
-  gp2x_ram = malloc(GP2X_RAM_SIZE);
+  gp2x_ram = calloc(1, GP2X_RAM_SIZE);
+  gp2x_mmio = calloc(1, 0x10000);
   
   pt_arm_cpu arm920;
   arm920.logging = true;
   pt_arm_init_cpu(&arm920, ARM920T,
-		  &bus_fetch_word, &bus_fetch_halfword, &bus_fetch_byte,
-		  &bus_write_word, &bus_write_halfword, &bus_write_byte);
+		  &bus_fetch, &bus_write);
 
   FILE* bootloader = fopen("data/gp2xboot.img", "rb+");
   if(bootloader == NULL) {
@@ -79,6 +85,9 @@ int main() {
   while((c = fgetc(stdin)) != 'q') {
     clock(&arm920);
   }
+  
+  tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+  printf("\n");
 
   SDL_DestroyRenderer(sdlRenderer);
   SDL_DestroyWindow(sdlWindow);
